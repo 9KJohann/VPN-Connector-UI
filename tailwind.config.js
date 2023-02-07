bb73de323e9bacd00b200977eb5578c396036f26
Johann Dietrich <johann.dietrich@miksim.de>
2023-02-07 11:59:08 +0100

    add color and format file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,20 @@
-const colors = require('tailwindcss/colors');
+const colors = require("tailwindcss/colors");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{html,js}'],
-	theme: {
-		colors: {
-			fore: colors.gray,
-			back: colors.indigo,
-			primary: colors.blue,
-			danger: colors.red,
-		},
-		extend: {},
-	},
-	plugins: [],
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      fore: colors.gray,
+      back: colors.indigo,
+      primary: colors.blue,
+      danger: colors.red,
+      green: colors.green,
+      yellow: colors.yellow,
+      red: colors.red,
+      blue: colors.blue,
+    },
+    extend: {},
+  },
+  plugins: [],
 };
